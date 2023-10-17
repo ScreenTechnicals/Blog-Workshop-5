58f33605f9d8a1cae43972ba8d8d82134dd227a5
anuragkmr45 <anuragkmr825@gmail.com>
2023-10-17 07:20:41 +0530

    dynamic routing

---
+++ src/app/blogs/[blog]/page.jsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+
+const Blog = ({ paramas }) => {
+
+    console.log(paramas)
+
+    return (
+        <>
+            <h1 className="text-2xl">Specific Blog Page</h1>
+            <br />
+            <h2 className="text-2xl">{ }</h2>
+        </>
+    )
+}
+
+export default Blog;
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Link from 'next/link'
 
 const Page = () => {
-  return <div>Blog Page</div>;
+
+  const Blogdata = [
+    {
+      title: 'Reactjs',
+      subTitle: 'React is a free and open-source front-end JavaScript library for building user interfaces based on components. It is maintained by Meta and a community of individual developers and companies. React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js',
+    },
+    {
+      title: 'Nextjs',
+      subTitle: 'Next.js is an open-source web development framework created by the private company Vercel providing React-based web applications with server-side rendering and static website generation.',
+    },
+  ]
+
+  return (
+    <>
+      <h1 className="text-3xl" >List of Blogs</h1>
+      <br />
+      <ul>
+        {Blogdata.map((data, index) => {
+          return (
+            <li key={index}>
+              <Link href={`/blogs/${data.title}`} >{data.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
 };
 
 export default Page;
